edb34744242d52b74fc331312088d9e4719dedb8
VikaIT <75212958+VikaIT@users.noreply.github.com>
2020-11-29 19:40:30 +0300

    Summary

---
+++ TaskApp/TaskApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TaskApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 0;
+			Task task = new Task(() => {
+				for (int i = 0; i < 100; i++)
+				{
+					a += i;
+				}
+				Console.WriteLine(a);
+			});
+			task.Start();
+			Console.ReadLine();
+		}
+	}
+}
